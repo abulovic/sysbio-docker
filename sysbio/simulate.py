1606d23a7c51ecb0a9a1e80559abe1c257449317
Ana Bulovic <bulovic.ana@gmail.com>
2016-06-28 19:42:00 +0200

    Implement model file transfer

--- sysbio/simulate.py
+++ sysbio/simulate.py
@@ -2,49 +2,59 @@
 import os
 import subprocess
 from argparse import ArgumentParser
+from contextlib import contextmanager
 
-_help = '''Script which envokes docker to simulate mathematical models using libroadrunner.
-(If configuration file is not provided, the script will try to find the [MODEL NAME.cfg
-file in the directory where the model is stored.)
-'''
+@contextmanager
+def create_delete(fname, mname):
+    with open(fname, 'w') as f:
+        f.write(mname)
+    yield
+    os.remove(fname)
+
 
 def get_parser():
-	parser = ArgumentParser()
-	parser.add_argument("MODEL", help="SBML xml file containing the model")
-	parser.add_argument("ODIR", help="Output directory where simulation results will be saved")
-	return parser
+    parser = ArgumentParser()
+    parser.add_argument("model", help="SBML xml file containing the model")
+    return parser
 
+_docker_cp = 'docker cp %(source)s data-store:/data/models/%(mname)s.%(type)s'
+_docker_cp_latest = 'docker cp latest data-store:/data/models/latest'
+
+
+def copy_files(mfile, cfile, mname):
+    # copy model file
+    _cp_model = _docker_cp % {
+        'source': mfile,
+        'mname': mname,
+        'type': 'xml'
+    }
+    subprocess.call(_cp_model)
+
+    # copy configuration file
+    if os.path.isfile(cfile):
+        _cp_cfg = _docker_cp % {
+            'source': cfile,
+            'mname': mname,
+            'type': 'cfg'
+        }
+        subprocess.call(_cp_cfg)
+
+    # write down the name of the model to simulate
+    # in the indicator file - /data/models/latest
+    with create_delete('latest', mname):
+        subprocess.call(_docker_cp_latest)
 
 def main():
-	parser = get_parser()
-	args = parser.parse_args()
 
-	model_dirs = args.MODEL.split(os.path.sep)
-	model_arg = './models/%s' % model_dirs[-1]
-	model_fname = '.'.join(model_dirs[-1].split('.')[:-1])
-	models_dir = os.path.sep.join(model_dirs[:-1])
-	models_dir = os.path.abspath(models_dir)
+    parser = get_parser()
+    args = parser.parse_args()
 
-	cfg_arg = ''
-	cfg_fname = './models/%s.cfg' % (model_fname, )
-	if os.path.isfile(cfg_fname):
-		cfg_arg = '--cfg %s' % cfg_fname
+    model_dir = '/'.join(args.model.split('/')[:-1])
+    model_name = '.'.join(args.model.split('/')[-1].split('.')[:-1])
+    cfg_file = '%s.cfg' % '/'.join([model_dir, model_name])
 
-	odir_arg = '--odir ./sim-output'
+    copy_files(args.model, cfg_file, model_name)
 
-	docker_cmd = 'docker run -ti -v %(host_models)s:/home/sysbio/models -v %(host_odir)s:/home/sysbio/sim-output abulovic/sysbio-docker' % {
-				 	'host_models': models_dir,
-				 	'host_odir': os.path.abspath(args.ODIR),
-				 }
-
-	exec_cmd = '%(docker_cmd)s ./simulator %(model_arg)s %(cfg_arg)s %(odir_arg)s' % {
-		'docker_cmd': docker_cmd,
-		'model_arg': model_arg,
-		'cfg_arg': cfg_arg,
-		'odir_arg': odir_arg
-	}
-
-	subprocess.call(exec_cmd, shell=True)
 
 if __name__ == '__main__':
-	main()
+    main()
