d4e094fdd2e6ccd970b5cbaa020dec52e0923ec8
Ana Bulovic <bulovic.ana@gmail.com>
2016-07-01 14:55:16 +0200

    Pass model name and format to all simulation functions

--- sysbio/simulate.py
+++ sysbio/simulate.py
@@ -6,27 +6,30 @@ from contextlib import contextmanager
 
 
 @contextmanager
-def create_delete(fname, mname):
+def create_delete(fname, mname, _format):
     with open(fname, 'w') as f:
         f.write(mname)
+        f.write('\n')
+        f.write(_format)
+        f.write('\n')
     yield
     os.remove(fname)
 
 
 def get_parser():
     parser = ArgumentParser()
-    parser.add_argument("model", help="SBML xml file containing the model")
+    parser.add_argument("model", help="Path to model file")
+    parser.add_argument("format", help="Model format", choices=['sbml', 'antimony'])
     return parser
 
 _docker_cp = 'docker cp %(source)s data-store:/data/models/'
 _docker_cp_latest = 'docker cp latest data-store:/data/models/latest'
 
 
-def copy_files(mfile, cfile, mname):
+def copy_files(mfile, cfile, mname, _format):
     # copy model file
     _cp_model = _docker_cp % {
         'source': mfile,
-        'type': 'xml'
     }
     p = subprocess.Popen(_cp_model.split(), stdout=subprocess.PIPE)
     out, err = p.communicate()
@@ -35,29 +38,21 @@ def copy_files(mfile, cfile, mname):
     if os.path.isfile(cfile):
         _cp_cfg = _docker_cp % {
             'source': cfile,
-            'type': 'cfg'
         }
         p = subprocess.Popen(_cp_cfg.split(), stdout=subprocess.PIPE)
         out, err = p.communicate()
 
-    # write down the name of the model to simulate
-    # in the indicator file - /data/models/latest
-    with create_delete('latest', mname):
-        subprocess.call(_docker_cp_latest, shell=True)
 
-def run_simulator():
-    _simulate_cmd = 'docker run --rm --volumes-from data-store sysbio-simulate python simulator sim'
+def run_simulator(model, _format):
+    _simulate_cmd = 'docker run --rm --volumes-from data-store sysbio-simulate python simulator %(m)s %(f)s sim' % {
+        'm': model,
+        'f': _format
+    }
     p = subprocess.Popen(_simulate_cmd.split(), stdout=subprocess.PIPE)
     out, err = p.communicate()
     return out.strip()
 
-def run_plot(outdir):
-    _plot_cmd = 'docker run --rm --volumes-from data-store sysbio-plot python create-notebook %s' % outdir
-    subprocess.call(_plot_cmd, shell=True)
-
-
-def run_simulation(model):
-
+def run_simulation(model, _format):
     model_dir = '/'.join(model.split('/')[:-1])
     model_name = '.'.join(model.split('/')[-1].split('.')[:-1])
     if model_dir:
@@ -65,40 +60,43 @@ def run_simulation(model):
     else:
         cfg_file = '%s.cfg' % model_name
 
-    copy_files(model, cfg_file, model_name)
+    copy_files(model, cfg_file, model_name, _format)
 
-    outdir = run_simulator()
+    d_model_file = '/data/models/%s' % model.split('/')[-1]
+    outdir = run_simulator(d_model_file, _format)
 
-    ls_cmd = 'docker run --rm --volumes-from data-store ubuntu ls %s' % outdir
-    p = subprocess.Popen(ls_cmd.split(), stdout=subprocess.PIPE)
-    out, err = p.communicate()
 
-    files = filter(lambda f: f.endswith('.txt'), out.strip().split())
+    plot_names = list_plots(d_model_file, _format)
+    plot_files = ['%s.txt' % name for name in plot_names]
 
-    for _f in files:
+    for _f in plot_files:
         cp_cmd = 'docker cp data-store:%(odir)s/%(file)s .'
         p = subprocess.Popen(cp_cmd % {
                 'odir': outdir,
                 'file': _f
             }, shell=True)
         out, err = p.communicate()
-    return list_plots(model)
+    return list_plots(d_model_file, _format)
 
-def list_plots(model):
+def list_plots(model, _format):
     model_dir = '/'.join(model.split('/')[:-1])
     model_name = '.'.join(model.split('/')[-1].split('.')[:-1])
     cfg_file = '%s.cfg' % '/'.join([model_dir, model_name])
 
-    copy_files(model, cfg_file, model_name)
-    _list_cmd = 'docker run --rm --volumes-from data-store sysbio-simulate python simulator list-plots'
+    copy_files(model, cfg_file, model_name, _format)
+    _list_cmd = 'docker run --rm --volumes-from data-store sysbio-simulate python simulator %(m)s %(f)s list-plots' % {
+        'm': model,
+        'f': _format
+    }
     p = subprocess.Popen(_list_cmd.split(), stdout=subprocess.PIPE)
     out, err = p.communicate()
-    return out.strip().split()
+    return out.strip().split('\n')
 
 def create_notebook():
     parser = get_parser()
     args = parser.parse_args()
     model = args.model
+    _format = args.format
 
     model_dir = '/'.join(model.split('/')[:-1])
     model_name = '.'.join(model.split('/')[-1].split('.')[:-1])
@@ -107,9 +105,13 @@ def create_notebook():
     else:
         cfg_file = '%s.cfg' % model_name
 
-    copy_files(model, cfg_file, model_name)
+    copy_files(model, cfg_file, model_name, _format)
 
-    _create_nb = 'docker run --rm --volumes-from data-store sysbio-plot python create-notebook %s' % model.split('/')[-1]
+    _create_nb = 'docker run --rm --volumes-from data-store sysbio-plot python create-notebook %(model)s %(format)s' %  {
+        'model': model.split('/')[-1],
+        'format': _format,
+    }
+
     p = subprocess.Popen(_create_nb.split(), stdout=subprocess.PIPE)
     out, err = p.communicate()
     nbfile = out.strip()
