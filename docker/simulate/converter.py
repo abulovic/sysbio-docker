74faee61261be0b818e8aba92b851b04670a3d11
Ana Bulovic <bulovic.ana@gmail.com>
2016-09-06 13:18:15 +0200

    Enable hierarchical model conversion.

--- docker/simulate/converter.py
+++ docker/simulate/converter.py
@@ -1,4 +1,4 @@
-import sys
+import sys, os
 from contextlib import nested
 
 import tellurium as te
@@ -12,11 +12,13 @@ converters = {
 
 
 
-model = '/data/models/to-convert'
+model = 'to-convert'
 src_dest = {
 	'src': sys.argv[1],
 	'dest': sys.argv[2],
 }
+
+os.chdir('/data/models/')
 
 conversion = '%(src)s:%(dest)s' % src_dest
 
@@ -24,6 +26,6 @@ if conversion not in converters:
 	raise Exception("Coversion from %(src)s to %(dest)s not available." % src_dest)
 else:
 	conv = converters[conversion]
-	converted_file = '/data/models/converted'
+	converted_file = 'converted'
 	with nested(open(model), open(converted_file, 'w')) as (fin, fout):
 		fout.write(conv(fin.read()))
