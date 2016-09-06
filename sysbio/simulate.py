f3df5d375d3171d4e68223e8c6eb62ceb62d3674
Ana Bulovic <bulovic.ana@gmail.com>
2016-09-06 13:17:48 +0200

    Moved function for file copying from simulator to utils

--- sysbio/simulate.py
+++ sysbio/simulate.py
@@ -4,7 +4,7 @@ import subprocess
 from argparse import ArgumentParser
 from contextlib import contextmanager
 
-from sysbio.utils import get_imported_models
+from sysbio.utils import get_imported_models, copy_files
 
 
 @contextmanager
@@ -26,46 +26,6 @@ def get_parser():
 
 _docker_cp = 'docker cp %(source)s data-store:/data/models/'
 _docker_cp_latest = 'docker cp latest data-store:/data/models/latest'
-
-
-def copy_files(mfile, cfile, mname, _format):
-
-    mdir = '/'.join(mfile.split('/')[:-1])
-
-    old_import_len = 0
-    if mdir:
-        imports = ['%s/%s' % (mdir, _mfile) for _mfile in get_imported_models(mfile)]
-    else:
-        imports = get_imported_models(mfile)
-    imports = set(imports)
-    while len(imports) != old_import_len:
-        old_import_len = len(imports)
-        new_imports = set()
-        for f in imports:
-            new_imports.update(get_imported_models(f))
-        imports.update(new_imports)
-
-    for imp in imports:
-        _cp_import = _docker_cp % {
-            'source': imp
-        }
-        p = subprocess.Popen(_cp_import.split(), stdout=subprocess.PIPE)
-        out, err = p.communicate()
-
-    # copy model file
-    _cp_model = _docker_cp % {
-        'source': mfile,
-    }
-    p = subprocess.Popen(_cp_model.split(), stdout=subprocess.PIPE)
-    out, err = p.communicate()
-
-    # copy configuration file
-    if os.path.isfile(cfile):
-        _cp_cfg = _docker_cp % {
-            'source': cfile,
-        }
-        p = subprocess.Popen(_cp_cfg.split(), stdout=subprocess.PIPE)
-        out, err = p.communicate()
 
 
 def run_simulator(model, _format):
--- sysbio/utils.py
+++ sysbio/utils.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 from argparse import ArgumentParser
 
@@ -39,6 +40,19 @@ def get_config_parser():
 def convert(mfile, src_fromat, dest_format):
 
 	mname = mfile.split('/')[-1].split('.')[0]
+	mdir = '/'.join(mfile.split('/')[:-1])
+
+	if mdir:
+		imports = ['%s/%s' % (mdir, _mfile) for _mfile in get_imported_models(mfile)]
+	else:
+		imports = get_imported_models(mfile)
+
+	for imp in imports:	
+		_docker_cp_to = 'docker cp %(mfile)s data-store:/data/models/to-convert' % {
+			'mfile': imp
+		}
+		subprocess.call(_docker_cp_to, shell=True)
+
 
 	_docker_cp_to = 'docker cp %(mfile)s data-store:/data/models/to-convert' % {
 		'mfile': mfile
@@ -50,7 +64,7 @@ def convert(mfile, src_fromat, dest_format):
 	}
 	subprocess.call(_docker_convert, shell=True)
 	_docker_cp_from = 'docker cp data-store:/data/models/converted %(mname)s.%(fend)s' % {
-		'mname': mname,
+		'mname': os.path.sep.join([mdir, mname]),
 		'fend': _file_endings[dest_format]
 	}
 	subprocess.call(_docker_cp_from, shell=True)
@@ -84,15 +98,55 @@ def run_create_default_config():
 	create_default_config(model)
 
 def get_imported_models(mfile):
-    import re
+	import re
 
-    imports = []
-    with open(mfile) as fin:
-        for line in fin:
-            if line.startswith('import'):
-                res = re.match('import +"(.+)"', line.strip())
-                if len(res.groups()) == 1:
-                    imports.append(res.groups()[0])
-            else:
-                continue
-    return imports
+	imports = []
+	with open(mfile) as fin:
+		for line in fin:
+			if line.startswith('import'):
+				res = re.match('import +"(.+)"', line.strip())
+				if len(res.groups()) == 1:
+					imports.append(res.groups()[0])
+			else:
+				continue
+	return imports
+
+
+def copy_files(mfile, cfile, mname, _format):
+
+	mdir = '/'.join(mfile.split('/')[:-1])
+
+	old_import_len = 0
+	if mdir:
+		imports = ['%s/%s' % (mdir, _mfile) for _mfile in get_imported_models(mfile)]
+	else:
+		imports = get_imported_models(mfile)
+	imports = set(imports)
+	while len(imports) != old_import_len:
+		old_import_len = len(imports)
+		new_imports = set()
+		for f in imports:
+			new_imports.update(get_imported_models(f))
+		imports.update(new_imports)
+
+	for imp in imports:
+		_cp_import = _docker_cp % {
+			'source': imp
+		}
+		p = subprocess.Popen(_cp_import.split(), stdout=subprocess.PIPE)
+		out, err = p.communicate()
+
+	# copy model file
+	_cp_model = _docker_cp % {
+		'source': mfile,
+	}
+	p = subprocess.Popen(_cp_model.split(), stdout=subprocess.PIPE)
+	out, err = p.communicate()
+
+	# copy configuration file
+	if os.path.isfile(cfile):
+		_cp_cfg = _docker_cp % {
+			'source': cfile,
+		}
+		p = subprocess.Popen(_cp_cfg.split(), stdout=subprocess.PIPE)
+		out, err = p.communicate()
